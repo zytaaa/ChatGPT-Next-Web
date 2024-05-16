e464d376c46249ae6c2da75f2e42f1f3b223f199
zytaaa <aiaalsavvinov20076@gmail.com>
2024-05-17 00:02:51 +0900

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -10,8 +10,8 @@ import { GoogleTagManager } from "@next/third-parties/google";
 const serverConfig = getServerSideConfig();
 
 export const metadata: Metadata = {
-  title: "NextChat",
-  description: "Your personal ChatGPT Chat Bot.",
+  title: "MaarGPT",
+  description: "Персональный чат бот нейросеть.",
   viewport: {
     width: "device-width",
     initialScale: 1,
